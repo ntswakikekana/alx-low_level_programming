32c61e98981c3c5376635097fa741867aef5813e
ntswakikekana <ntswakikekana46@gmail.com>
2025-03-21 01:56:32 -0700

    syntax correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point of program
+ * main - Entry point of the program
  *
  * Description: This program prints the alphabet in lowercase,
- * follwed by a new line, using only putchar funtion.
+ * followed by a new line, using only the putchar function.
  *
- * Return: Always 0 (succcess)
- **/
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char letter;
-	for (letter 'a', letter <= 'z',letter ++)
-		putchar(letter);
-	putchar('\n');
+    char letter;
 
-	return(0);
+    for (letter = 'a'; letter <= 'z'; letter++)
+        putchar(letter);
+    
+    putchar('\n');
 
+    return (0);
 }
