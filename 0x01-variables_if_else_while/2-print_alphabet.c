ed1ba4395492c4086b1b36f9f910b322887f2581
ntswakikekana <ntswakikekana46@gmail.com>
2025-03-21 01:47:23 -0700

    print-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * follwed by a new line, using only putchar funtion.
+ *
+ * Return: Always 0 (succcess)
+ **/
+int main (void)
+{
+	char letter;
+	for (letter 'a', letter <= 'z',letter ++)
+		putchar(letter);
+	putchar('\n');
+
+	return(0);
+
+}
